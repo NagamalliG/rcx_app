b3406dd7a944a99981da685ebc5a34464231cb46
NagamalleswaraRao <ngannavarapu@rythmos.com>
2017-03-10 20:17:45 +0530

    written logic for get the shop name and inserted the shop name into db.

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,7 +5,9 @@ class WebhooksController < ShopifyApp::WebhooksController
  end
   def productCreate
       @success = 'success'
-    #   Customers.connection.insert("INSERT INTO customers (id,first_name,last_name,points) VALUES (#{@id},'#{@first_name}','#{@last_name}',#{@points});")
+      
+     puts request.headers["HTTP_X_SHOPIFY_SHOP_DOMAIN"]
+    #   Customerdetails.connection.insert("INSERT INTO customers (id,first_name,last_name,points) VALUES (#{@id},'#{@first_name}','#{@last_name}',#{@points});")
       render status: 200 , json: @success.to_json
  end
  def customersCreate
@@ -13,11 +15,13 @@ class WebhooksController < ShopifyApp::WebhooksController
       @first_name = params[:first_name]
       @last_name = params[:last_name]
       @points = 100
+      @shop_name = request.headers["HTTP_X_SHOPIFY_SHOP_DOMAIN"]
     #   puts @id 
     #   puts @first_name
     #   puts @last_name
     #   puts @points
-      Customers.connection.insert("INSERT INTO customers (id,first_name,last_name,points) VALUES (#{@id},'#{@first_name}','#{@last_name}',#{@points});")  
+    #   puts @shop_name
+      Customerdetails.connection.insert("INSERT INTO customerdetails (id,shop_name,first_name,last_name,points) VALUES (#{@id},'#{@shop_name}','#{@first_name}','#{@last_name}',#{@points});")
       @success = 'success'
      render status: 200 , json: @success.to_json
  end
