955a01e7712679af6fb6ac06b4a42f5e95353a11
Rambabu Chimata <rchimata@loyaltymethods.com>
2017-03-15 12:23:54 +0530

    CHanged webhooks to heroku end point

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,12 +1,12 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "RCX Loyalty"
   config.api_key = "f1bed68735c1a791cb1d4f7bb89bf126"
   config.secret = "7308644ba27901d832cba146d14b3254"
   config.scope = "read_orders,read_products,read_customers"
   config.embedded_app = true
   config.webhooks = [
-    {topic: 'customers/create', address: 'https://13dcd233.ngrok.io/webhooks/customers_create', format: 'json'},
-    {topic: 'products/create', address: 'https://13dcd233.ngrok.io/webhooks/product_create', format: 'json'},
-    {topic: 'carts/update', address: 'https://13dcd233.ngrok.io/webhooks/carts_update', format: 'json'}
+    {topic: 'customers/create', address: 'https://still-atoll-94279.herokuapp.com/webhooks/customers_create', format: 'json'},
+    {topic: 'products/create', address: 'https://still-atoll-94279.herokuapp.com/webhooks/product_create', format: 'json'},
+    {topic: 'carts/update', address: 'https://still-atoll-94279.herokuapp.com/webhooks/carts_update', format: 'json'}
   ]
 end
