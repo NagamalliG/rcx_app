f739551366b88cde4e7d41087297db9098596112
NagamalleswaraRao <ngannavarapu@rythmos.com>
2017-03-15 16:54:46 +0530

    Reverting to sqllite and ngrok

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,12 +1,12 @@
 ShopifyApp.configure do |config|
-  config.application_name = "RCX Loyalty"
-  config.api_key = "f1bed68735c1a791cb1d4f7bb89bf126"
-  config.secret = "7308644ba27901d832cba146d14b3254"
+  config.application_name = "My Shopify App"
+  config.api_key = "39bf3276a42977acd720798ebfc17809"
+  config.secret = "c3d91fda446c565877bf988a15485d5f"
   config.scope = "read_orders,read_products,read_customers"
   config.embedded_app = true
   config.webhooks = [
-    {topic: 'customers/create', address: 'https://still-atoll-94279.herokuapp.com/webhooks/customers_create', format: 'json'},
-    {topic: 'products/create', address: 'https://still-atoll-94279.herokuapp.com/webhooks/product_create', format: 'json'},
-    {topic: 'carts/update', address: 'https://still-atoll-94279.herokuapp.com/webhooks/carts_update', format: 'json'}
+    {topic: 'customers/create', address: 'https://f94b86dc.ngrok.io/webhooks/customers_create', format: 'json'},
+    {topic: 'products/create', address: 'https://f94b86dc.ngrok.io/webhooks/product_create', format: 'json'},
+    {topic: 'carts/update', address: 'https://f94b86dc.ngrok.io/webhooks/carts_update', format: 'json'}
   ]
 end
